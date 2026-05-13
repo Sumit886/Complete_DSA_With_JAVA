8c45b6d66c65618e0b705c59ae3ba4e801f08f5f
Sumit886 <sk1019405@gmail.com>
2026-05-13 08:57:57 +0530

    Add twoSum method to find indices of two numbers
    Implement twoSum method using a hashmap to return indices of the two elements that add up to the target.

--- Maps&Sets/TwoSum.java
+++ Maps&Sets/TwoSum.java
@@ -11,3 +11,25 @@ public class TwoSum {
         return false;
     }
 }
+
+
+  //Using hashmap try we have to return the index of the two elements in an aaray
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int rem = target - nums[i];
+
+            if(map.containsKey(rem)) {
+                return new int[]{ map.get(rem), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
